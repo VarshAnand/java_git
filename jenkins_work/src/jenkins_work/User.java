1a5ee26ceead4e3c629b4049721b35c2c96ca81b
Varsha Anand <aanandvarsha2000@gmail.com>
2024-10-14 16:51:48 +0530

    commit java file

---
+++ jenkins_work/src/jenkins_work/User.java
@@ -0,0 +1,15 @@
+package jenkins_work;
+
+public class User {
+
+
+	  public static void main(String[] args) {
+	    
+	    int first = 10;
+	    int second = 20;
+
+	    // add two numbers
+	    int sum = first + second;
+	    System.out.println(first + " + " + second + " = "  + sum);
+	  }
+	}
